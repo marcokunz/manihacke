a6c109ab57a8f1f5aacf86142a2bfb83ddf66427
marcokunz <takuma@bluewin.ch>
2018-03-16 13:30:32 +0100

    update

--- manihacke/src/manihacke/BankVCT.java
+++ manihacke/src/manihacke/BankVCT.java
@@ -1,9 +1,46 @@
 package manihacke;
 
-public class BankVCT {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BankVCT {
+	
+
+
+public static void main(String[] args) throws SQLException, InstantiationException, IllegalAccessException, ClassNotFoundException {
+		
+		String url = "jdbc:mysql://sql11.freesqldatabase.com:3306/";
+		String dbName = "sql11226963";
+
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "sql11226963"; 
+		String password = "A6DJWbxVgN";
+
+		Class.forName(driver).newInstance();
+		    Connection conn = DriverManager.getConnection(url+dbName,userName,password);
+		    Statement stmt = conn.createStatement() ;
+		    ResultSet rs;
+		    
+		    rs = stmt.executeQuery("SELECT * FROM Account");
+		    System.out.println("CustomerID\tCustomerName\tStreetName\tZIP\tTown\tCountry\tTypeOfCustomer\tAccountNumber\tTotal\tClearing");
+			while (rs.next()) {
+				String CustomerID = rs.getString("CustomerID");
+				String CustomerName = rs.getString("CustomerName");
+				String StreetName = rs.getString("StreetName");
+				String ZIP = rs.getString("ZIP");
+				String Town = rs.getString("Town");
+				String Country = rs.getString("Country");
+				String TypeOfCustomer = rs.getString("TypeOfCustomer");
+				String AccountNumber = rs.getString("AccountNumber");
+				String Total = rs.getString("Total");
+				String Clearing = rs.getString("Clearing");
+				System.out.println("\t" + CustomerID + "\t" + CustomerName + "\t" + StreetName + "\t" + ZIP + "\t" + Town + "\t" + Country + "\t" + TypeOfCustomer + "\t" + AccountNumber + "\t" + Total + "\t" + Clearing);
+			}
+		
 
 	}
 
